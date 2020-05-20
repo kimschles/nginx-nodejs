f57fb9dd418d35b1af0bdda536d4ac7e31bd7ab6
kimschles <schlesinger.kim@gmail.com>
2020-05-19 21:58:09 -0600

    update server code

--- nodejs-server/app.js
+++ nodejs-server/app.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const express = require('express');
+const moment = require('moment');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const TIME = moment().format('MMMM Do YYYY, h:mm:ss a')
+const currentTime = `This Node.js server started at ${TIME}`;
 
 // App
 const app = express();
 app.use(express.json())
 app.get('/', (req, res) => {
-    res.send({"message": "Hello World"});
+    res.send({"message": "Hello Denver Node.js!", "time": currentTime});
 });
 
 app.listen(PORT, HOST);
